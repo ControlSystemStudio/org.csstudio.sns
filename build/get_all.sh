b6642aff528fec3349e5ca853d6c8dcd529f97ec
Kay Kasemir <kasemirk@ornl.gov>
2016-04-01 10:10:14 -0400

    get_all: Fix

--- build/get_all.sh
+++ build/get_all.sh
@@ -1,7 +1,14 @@
 GH=https://github.com/ControlSystemStudio/
 for i in diirt maven-osgi-bundles cs-studio-thirdparty cs-studio org.csstudio.sns
 do
-   git clone $GH
+  if [ -d $i ]
+  then
+    echo "==== Updating $i ===="
+    (cd $i; git pull)
+  else
+    echo "==== Fetching $i ===="
+    git clone $GH/$i
+  fi
 done
 
 
