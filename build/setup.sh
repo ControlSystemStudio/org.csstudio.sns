bc7642011dfdd4c0f0b645766f3d855b8ec8d686
kasemir <kasemirk@ornl.gov>
2016-08-03 14:14:44 -0400

    Build setup

--- build/setup.sh
+++ build/setup.sh
@@ -1,5 +1,5 @@
-export JAVA_HOME=$HOME/Eclipse/jdk1.8.0_60
-export M2_HOME=$HOME/Eclipse/apache-maven-3.2.3
+export JAVA_HOME=`echo $HOME/Eclipse/jdk1.8*`
+export M2_HOME=`echo $HOME/Eclipse/apache-maven-3.*`
 
 export SRC_DIR=`(cd ../..; pwd)`
 
