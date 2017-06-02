04a03d5a2fcf2325f7f8b158858300360b5c9960
Kay Kasemir <kasemirk@ornl.gov>
2017-06-02 16:57:17 -0400

    target ideas...

--- build/mk_target.sh
+++ build/mk_target.sh
@@ -1,45 +1,28 @@
 #!/bin/sh
 #
-# Example for collecting the files for a "target" platform,
-# i.e. the same files which we now get via maven - on a good day.
+# Turn target_platform with plugins/ and features/ into P2 repo.
 
-mkdir -p tgt
-cd tgt
+ECLIPSE=~/Eclipse/4.5.2/Eclipse.app/Contents/Eclipse
 
-# Collect all plugins, for all architectures
-cp -r ../../repository/target/products/org.csstudio.sns.product.product/linux/gtk/x86_64/sns-css-4.2.0/plugins/* .
-cp -r ../../repository/target/products/org.csstudio.sns.product.product/linux/gtk/x86/sns-css-4.2.0/plugins/* .
-cp -r ../../repository/target/products/org.csstudio.sns.product.product/win32/win32/x86/sns-css-4.2.0/plugins/* .
-cp -r ../../repository/target/products/org.csstudio.sns.product.product/macosx/cocoa/x86_64/Css4.2.0.app/Contents/Eclipse/plugins/* .
+LAUNCHER=`echo "${ECLIPSE}/plugins/org.eclipse.equinox.launcher_*.jar"`
 
-for p in *
-do
-  if [ -r ~/Eclipse/4.5.2/Eclipse.app/Contents/Eclipse/plugins/$p ]
-  then
-     echo "$p - IDE"
-     rm -rf $p
-  fi
-  
-  C=`find ../../../cs-studio -name $p | wc -l`
-  if [ $C -gt 0 ]
-  then
-      echo "$p - cs-studio"
-      rm -rf $p
-  fi
+TARGET_PLATFORM=`pwd`/target_platform
 
-  case $p
-  in
-  *sns*)
-      echo "$p - SNS"
-      rm -rf $p
-  ;;
-  org.csstudio.diag.pvutil_*)
-      echo "$p - SNS"
-      rm -rf $p
-  ;;
-  org.csstudio.diag.rack_*)
-      echo "$p - SNS"
-      rm -rf $p
-  ;;
-  esac
-done
+echo "Creating P2 repository for $TARGET_PLATFORM"
+rm -f $TARGET_PLATFORM/artifacts.xml $TARGET_PLATFORM/content.xml
+java -jar $LAUNCHER \
+   -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
+   -source $TARGET_PLATFORM \
+   -metadataRepository file:/$TARGET_PLATFORM \
+   -artifactRepository file:/$TARGET_PLATFORM \
+   -publishArtifacts \
+   -configs cocoa.macosx.x86_64
+
+#   -compress \
+#   -configs gtk.linux.x86
+
+echo "Contents of repository:"
+java -jar $LAUNCHER \
+   -application org.eclipse.equinox.p2.director \
+   -repository file:/$TARGET_PLATFORM \
+   -list
