a248431296a1e2c598d9b1e22e6d5d8aea7d18c7
Kay Kasemir <kasemirk@ornl.gov>
2017-06-04 09:12:56 -0400

    mk_target: input different from target

--- build/mk_target.sh
+++ build/mk_target.sh
@@ -6,23 +6,19 @@ ECLIPSE=~/Eclipse/4.5.2/Eclipse.app/Contents/Eclipse
 
 LAUNCHER=`echo "${ECLIPSE}/plugins/org.eclipse.equinox.launcher_*.jar"`
 
+TARGET_INPUT=`pwd`/target_input
 TARGET_PLATFORM=`pwd`/target_platform
 
 echo "Creating P2 repository for $TARGET_PLATFORM"
-rm -f $TARGET_PLATFORM/artifacts.xml $TARGET_PLATFORM/content.xml
+rm -rf $TARGET_PLATFORM
 java -jar $LAUNCHER \
    -application org.eclipse.equinox.p2.publisher.FeaturesAndBundlesPublisher \
-   -source $TARGET_PLATFORM \
+   -source $TARGET_INPUT \
    -metadataRepository file:/$TARGET_PLATFORM \
    -artifactRepository file:/$TARGET_PLATFORM \
    -publishArtifacts \
    -configs cocoa.macosx.x86_64
 
-#   -compress \
 #   -configs gtk.linux.x86
+#   -compress \
 
-echo "Contents of repository:"
-java -jar $LAUNCHER \
-   -application org.eclipse.equinox.p2.director \
-   -repository file:/$TARGET_PLATFORM \
-   -list
