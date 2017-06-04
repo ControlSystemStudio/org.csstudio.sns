0d3bfec21b1c9eaf9aa0b1823eac8cee3806574c
Kay Kasemir <kasemirk@ornl.gov>
2017-06-04 13:04:53 -0400

    Updating to neon...

--- build/make.sh
+++ build/make.sh
@@ -53,23 +53,29 @@ cd ../..
 TOP=`pwd`
 
 # rm -rf ~/.m2/repository
-rm -f 0_diirt.log 1_maven-osgi-bundles.log 2_cs-studio-thirdparty.log  3_core.log 4_applications.log 5_display_builder.log 6_sns.log
+# rm -f 0_diirt.log 1_maven-osgi-bundles.log 2_cs-studio-thirdparty.log  3_core.log 4_applications.log 5_display_builder.log 6_sns.log
 
-(cd diirt; time  mvn $OPTS ) | tee 0_diirt.log
+# With download: Total time: 02:40 min
+# (cd diirt; time  mvn $OPTS ) 2>&1 | tee 0_diirt.log
 
-(cd maven-osgi-bundles; time  mvn $OPTS ) | tee 1_maven-osgi-bundles.log
+# With download: Total time: 04:36 min
+# (cd maven-osgi-bundles; time  mvn $OPTS ) 2>&1 | tee 1_maven-osgi-bundles.log
 
-(cd cs-studio-thirdparty; time  mvn $OPTS ) | tee 2_cs-studio-thirdparty.log
+# With download: Total time: 03:00 min
+# (cd cs-studio-thirdparty; time  mvn $OPTS ) 2>&1 | tee 2_cs-studio-thirdparty.log
 
-(cd cs-studio/core; time  mvn $OPTS ) | tee 3_core.log
+# With download: Total time: 07:12 min
+# (cd cs-studio/core; time  mvn $OPTS ) 2>&1 | tee 3_core.log
 
-(cd cs-studio/applications; time  mvn $OPTS ) | tee 4_applications.log
+# With download: Total time: 12:24 min
+# (cd cs-studio/applications; time  mvn $OPTS ) 2>&1 | tee 4_applications.log
 
 # display.builder pom.xml looks for $CSS_REPO
-CSS_REPO=file:$TOP/org.csstudio.sns/css_repo
-(cd org.csstudio.display.builder; time mvn -s $MSET -Dcss-repo=$CSS_REPO clean verify ) | tee 5_display_builder.log
+# CSS_REPO=file:$TOP/org.csstudio.sns/css_repo
+# (cd org.csstudio.display.builder; time mvn -s $MSET -Dcss-repo=$CSS_REPO clean verify ) 2>&1 | tee 5_display_builder.log
 
-(cd org.csstudio.sns; time  mvn $OPTS ) | tee 6_sns.log
+# With download: 
+(cd org.csstudio.sns; time  mvn $OPTS ) 2>&1 | tee 6_sns.log
 
 # Products now under org.csstudio.sns/repository/target/products/*
 # Feature repo under org.csstudio.sns/repository/target/repository
